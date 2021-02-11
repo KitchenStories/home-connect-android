35ba0f2a9fc48fe65ae51df5b3579c27edce50b4
aparna <aparnapattathilotmail.com>
2021-02-11 18:48:10 +0530

    make the program constraints nullable

--- home-connect/src/main/java/com/ajnsnewmedia/kitchenstories/homeconnect/model/programs/SpecificAvailableProgram.kt
+++ home-connect/src/main/java/com/ajnsnewmedia/kitchenstories/homeconnect/model/programs/SpecificAvailableProgram.kt
@@ -5,23 +5,24 @@ import com.squareup.moshi.JsonClass
 @JsonClass(generateAdapter = true)
 data class SpecificAvailableProgram(
     val key: String,
-    val name: String,
+    val name: String?,
     val options: List<ProgramOptions>
 )
 
 @JsonClass(generateAdapter = true)
 data class ProgramOptions(
     val key: String,
-    val name: String,
+    val name: String?,
     val type: String,
-    val unit: String,
+    val unit: String?,
     val constraints: ProgramConstraints
 )
 
 @JsonClass(generateAdapter = true)
 data class ProgramConstraints(
-    val min: Int,
-    val max: Int,
-    val stepSize: Int,
-    val allowedValues: List<String>
+    val min: Int?,
+    val max: Int?,
+    val default: Any?,
+    val stepsize: Int?,
+    val allowedvalues: List<String>?
 )
